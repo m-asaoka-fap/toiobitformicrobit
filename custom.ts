75892884665dbc8a70d6a58e3b3ea1d3a52812ec
m-asaoka-fap <66696898+m-asaoka-fap@users.noreply.github.com>
2023-05-08 21:31:15 +0900

    更新 pxt.json, main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -6,13 +6,92 @@
  * 2023.05.08  (FAP)m-asaoka  新規作成
  *
  */
+/* プリセットされた音 */
+enum SoundEffect {
+    //% block="0(Enter)"
+    enter = 0,
+    //% block="1(selected)"
+    selected = 1,
+    //% block="2(cancel)"
+    cancel = 2,
+    //% block="3(cursor)"
+    cursor = 3,
+    //% block="4(matin)"
+    matin = 4,
+    //% block="5(matout)"
+    matout = 5,
+    //% block="6(get1)"
+    get1 = 6,
+    //% block="7(get2)"
+    get2 = 7,
+    //% block="8(get3)"
+    get3 = 8,
+    //% block="9(effect1)"
+    effect1 = 9,
+    //% block="10(effect2)"
+    effect2 = 10
+}
+/* Waitする時間 */
+enum WaitTime {
+    //% block="1秒（びょう）"
+    WAIT1 = 1000,
+    //% block="5秒（びょう）"
+    WAIT2 = 5000,
+    //% block="10秒（びょう）"
+    WAIT3 = 10000,
+    //% block="30秒（びょう）"
+    WAIT4 = 30000,
+    //% block="60秒（びょう）"
+    WAIT5 = 60000
+}
+/* モーター回転数*/
+enum ThrottleKind {
+    //% block="10"
+    TH10 = 10,
+    //% block="15"
+    TH15 = 15,
+    //% block="20"
+    TH20 = 20,
+    //% block="25"
+    TH25 = 25,
+    //% block="30"
+    TH30 = 30,
+    //% block="35"
+    TH35 = 35,
+    //% block="40"
+    TH40 = 40,
+    //% block="45"
+    TH45 = 45,
+    //% block="50"
+    TH50 = 50,
+    //% block="55"
+    TH55 = 55,
+    //% block="60"
+    TH60 = 60,
+    //% block="65"
+    TH65 = 65,
+    //% block="70"
+    TH70 = 70,
+    //% block="75"
+    TH75 = 75,
+    //% block="80"
+    TH80 = 80,
+    //% block="85"
+    TH85 = 85,
+    //% block="90"
+    TH90 = 90,
+    //% block="95"
+    TH95 = 95,
+    //% block="100"
+    TH100 = 100
+}
 /**
  * カスタムブロック
  */
-//% weight=100 color=#0fbc11 icon="" block="toio:bit"
+//% weight=0 color=#0fbc11 icon="" block="toio:bit"
 namespace custom {
-    let bInit = false;          // 初期化完了かどうか
-    let defaultThrottle = 10;   // デフォルトのストットル値
+    let bInit = false;                         // 初期化完了かどうか
+    let defaultThrottle = ThrottleKind.TH10;   // デフォルトのストットル値
     /**
     *  Toioを使うための設定を行います
     */
@@ -21,8 +100,8 @@ namespace custom {
     export function start(): void {
         // シリアル通信をセットアップする
         serial.redirect(
-            SerialPin.P8,
             SerialPin.P12,
+            SerialPin.P0,
             BaudRate.BaudRate57600
         )
         bInit = true;
@@ -80,6 +159,18 @@ namespace custom {
         sendText(strSendTxt);
     }
     /**
+    * 停止します
+    */
+    //% block="×"
+    //% group="レベル１"
+    export function stop(): void {
+        if (bInit != true) {
+            return;
+        }
+        let strSendTxt = "stop";
+        sendText(strSendTxt);
+    }
+    /**
     * チャルメラ♪
     */
     //% block="♪ちゃるめら♪"
@@ -92,11 +183,23 @@ namespace custom {
         sendText(strSendTxt);
     }
     /**
+    * 効果音（こうかおん）を設定（せってい）するよ
+    */
+    //% block="効果音（こうかおん） %value がなる"
+    // SoundEffect.defl = SoundEffect.enter
+    //% group="レベル１"
+    export function soundEffect(effect:SoundEffect): void {
+        if (bInit != true) {
+            return;
+        }
+        let strEffectType = convertToText(effect);
+        let strSendTxt = "presetsound=" + strEffectType;
+        sendText(strSendTxt);
+    }
+    /**
     *  文字列をシリアル通信ポートに送信します
     */
     export function sendText(sendText:string) {
-        serial.writeString(sendText);
+        serial.writeLine(sendText);
     } 
-    
-    
 }
--- main.ts
+++ main.ts
@@ -3,14 +3,22 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
 })
 input.onButtonPressed(Button.A, function () {
     custom.left()
+    custom.soundEffect(SoundEffect.enter)
 })
 input.onGesture(Gesture.LogoUp, function () {
     custom.back()
+    custom.soundEffect(SoundEffect.get2)
 })
 input.onGesture(Gesture.LogoDown, function () {
     custom.forward()
+    custom.soundEffect(SoundEffect.cursor)
+})
+input.onButtonPressed(Button.AB, function () {
+    custom.stop()
+    custom.soundEffect(SoundEffect.effect1)
 })
 input.onButtonPressed(Button.B, function () {
     custom.right()
+    custom.soundEffect(SoundEffect.get1)
 })
 custom.start()
